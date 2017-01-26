596c454b6a0e04f7ebf4120710f979d61ba04338
Tiago Versuti <tiagoversuti@gmail.com>
2017-01-26 11:15:12 -0200

    2 | ASP.NET Core and Middleware     Video: Application Configurations and NuGet Packages     Video: ASP.NET Core and Middleware

---
+++ src/MariaLib/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MariaLib
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+
+        public static string Greeting()
+        {
+            return "Hello from MariaLib!";
+        }
+    }
+}
