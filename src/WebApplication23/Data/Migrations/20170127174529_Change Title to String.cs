04e107ca8eedd1b134ee12d961cb4a2b4cba94d4
Tiago Versuti <tiagoversuti@gmail.com>
2017-01-27 17:53:36 -0200

    4 | Creating a Form     Video: Creating a Form     Video: Form Validation

---
+++ src/WebApplication23/Data/Migrations/20170127174529_Change Title to String.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication23.Data.Migrations
+{
+    public partial class ChangeTitletoString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Title",
+                table: "Movie",
+                nullable: false);
+        }
+    }
+}
